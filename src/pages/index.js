830981e2a87dc98fb0c1cb5f3bb4c6b00bbdf5d2
Noor Mohammed <noormohammedknr2@gmail.com>
2023-04-10 20:54:37 +0530

    installation for next js for protofolio done

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <div>This is the next js protofolio webiste of Noor's</div>;
+}
